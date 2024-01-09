5ce266bb15b90f9e8ff0a470a45c9f42ae1acb45
Kassia Leão <kassiadarcie@gmail.com>
2024-01-09 10:04:39 -0300

    feat: implemented the budget logic
    addet funcionality to edit the budget and subtracts from the sum of checked products

--- src/app/components/Budge.tsx
+++ src/app/components/Budge.tsx
@@ -1,8 +1,49 @@
+import { useState } from 'react';
+import { FiEdit } from 'react-icons/fi';
+import { useProducts } from '../contexts/ProductsContext';
+
 export const Budge = () => {
+	const productCtx = useProducts();
+	const [editing, setEditing] = useState(false);
+	const sumChecked: number = productCtx
+		? productCtx?.products.reduce<number>((acc, current) => {
+				if (current.checked === true) {
+					return acc + current.price;
+				}
+				return acc;
+		  }, 0)
+		: 0;
+
+	const handleEnterKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') setEditing(false);
+	};
 	return (
 		<div>
 			<h3 className="text-sm mb-1">Orçamento disponível</h3>
-			<p className="text-4xl">R$ 00,00</p>
+
+			<div className="flex gap-2">
+				{!editing ? (
+					<p className="text-4xl">
+						R$ {productCtx && `${(productCtx.budget - sumChecked).toFixed(2)}`}
+					</p>
+				) : (
+					<input
+						type="number"
+						className="text-4xl"
+						value={productCtx?.budget}
+						onChange={(e) => productCtx?.setBudget(Number(e.target.value))}
+						onKeyUp={handleEnterKey}
+					/>
+				)}
+				{!editing && (
+					<FiEdit
+						className="text-blue-500 hover:text-blue-600"
+						size=".8rem"
+						onClick={() => setEditing(true)}
+					/>
+				)}
+			</div>
+			<p>TOTAL: R$ {sumChecked.toFixed(2)}</p>
 		</div>
 	);
 };
